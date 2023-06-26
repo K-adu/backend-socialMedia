128f704d85f81bee61f05f5de2258d48c3c656ca
K-adu <manishbaral112@gmail.com>
2023-06-26 17:20:50 +0545

    :use of axios

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,19 @@
 
+window.onload = function() {
+    axios.get(`/posts`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+        // Display the data on the frontend as needed
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
 // Save the token to local storage
 function saveTokenToLocalStorage(token) {
